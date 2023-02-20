d537081e38a74ba5ad38f3fa0b9eab330d18443e
prataprohit526 <prataprohit526@gmail.com>
2023-02-20 14:33:45 +0530

    all change

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,21 @@ import '../styles/App.css';
 
 const App = () => {
 //code here
-const [preValue, newvalue]=useState("redColor");
-let temp=1;
-function fun(){
-  if(temp){
-    temp=0;
-  newvalue('blueColor');
- 
-  // console.log(temp);
-  }else{
-newvalue('redColor');
-temp=1;
-//console.log(temp);
-  }
-}
-  return (
-    <div id="main">
-      <p className={preValue} >Newton School</p>
-      <button id='button' onClick={fun}>Change Style</button>
-    </div>
-  )
+const [colorClass, setColorClass] = useState(false);
+    const changeStyle = () => {
+        const currentState = colorClass;
+        setColorClass(!currentState);
+    };
+    return (
+        <div id="main">
+            <p className={colorClass ? "blueColor" : "redColor"}>
+                Newton School
+            </p>
+            <button id="button" onClick={changeStyle}>
+                Change Style
+            </button>
+        </div>
+    );
 }
 
 
