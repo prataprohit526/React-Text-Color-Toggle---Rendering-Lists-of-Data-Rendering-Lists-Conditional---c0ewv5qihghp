05462e22b932c9db9d41095d3ac5f86ad80b0e37
prataprohit526 <prataprohit526@gmail.com>
2023-02-20 14:08:47 +0530

    add file

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+//code here
+const [preValue, newvalue]=useState("redColor");
+let temp=1;
+function fun(){
+  if(temp){
+    temp=0;
+  newvalue('blueColor');
+ 
+  // console.log(temp);
+  }else{
+newvalue('redColor');
+temp=1;
+//console.log(temp);
+  }
+}
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={preValue} >Newton School</p>
+      <button id='button' onClick={fun}>Change Style</button>
     </div>
   )
 }
